2d2e35611a03bb2e927dbd6460cf680329bf7395
Matias Leal <77634804+Meikez@users.noreply.github.com>
2024-04-26 12:34:28 -0300

    Delete script directory

--- script/script.js
+++
@@ -1,8 +0,0 @@
-const toggleMenu = document.getElementById('toggle-menu');
-const menuList = document.getElementById('menu-list');
-
-toggleMenu.addEventListener('click', () => {
-    menuList.classList.toggle('show-menu');
-    toggleMenu.classList.toggle('show-menu-icon')
-});
-
